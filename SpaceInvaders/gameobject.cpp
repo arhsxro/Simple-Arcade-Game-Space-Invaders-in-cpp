618dd77ad65081029c835b94d9c49cf60ef422f2
Aris Chronopoulos <arhsxro@gmail.com>
2021-10-12 12:17:20 +0300

    first commmit

---
+++ SpaceInvaders/gameobject.cpp
@@ -0,0 +1,8 @@
+#include "gameobject.h"
+#include "Game.h"
+
+GameObject::GameObject(const Game& mygame)
+	:game(mygame)
+{
+
+}
---
+++ SpaceInvaders/gameobject.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class GameObject
+{
+	const class Game& game;
+public:
+	
+	GameObject(const class Game& mygame);
+	virtual void update() = 0;
+	virtual void draw() = 0;
+	virtual void init() = 0;
+};
